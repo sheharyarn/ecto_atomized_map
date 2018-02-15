45c723b312fe7ed57ae1a18d0345f574a16d2268
Sheharyar Naseer <hello@sheharyar.me>
2018-02-15 18:20:16 -0500

    Clean up mix

--- mix.exs
+++ mix.exs
@@ -1,15 +1,36 @@
 defmodule Ecto.AtomizedMap.Mixfile do
   use Mix.Project
 
+  @app     :ecto_atomized_app
+  @name    "Ecto.AtomizedApp"
+  @version "0.1.0"
+  @github  "https://github.com/sheharyarn/#{@app}"
+  @author  "Sheharyar Naseer"
+  @license "MIT"
+
+
   def project do
     [
-      app: :ecto_atomized_map,
-      version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
+      # Project
+      app:          @app,
+      version:      @version,
+      elixir:       "~> 1.3",
+      description:  description(),
+      package:      package(),
+      deps:         deps(),
+
+      # ExDoc
+      name:         @name,
+      source_url:   @github,
+      homepage_url: @github,
+      docs: [
+        main:       @name,
+        canonical:  "https://hexdocs.pm/#{@app}",
+        extras:     ["README.md"]
+      ]
     ]
   end
+
 
   def application do
     [extra_applications: [:logger]]
@@ -22,5 +43,21 @@ defmodule Ecto.AtomizedMap.Mixfile do
       {:ex_utils, "~> 0.1.4"},
     ]
   end
-end
 
+
+  defp description do
+    "Use Maps with Atom Keys in Ecto Schemas"
+  end
+
+
+  defp package do
+    [
+      name: @app,
+      maintainers: [@author],
+      licenses: [@license],
+      files: ~w(mix.exs lib README.md),
+      links: %{"Github" => @github}
+    ]
+  end
+
+end
